d7d463bc55b7f335525d1ab8398224f9bf272561
Manoj Kumar <manoj.er.kumar@gmail.com>
2021-08-17 13:59:58 +0530

    calling run method directly , does not create new Thread, it is still single thread

---
+++ src/com/thread/Mythread1.java
@@ -0,0 +1,17 @@
+package com.thread;
+
+public class Mythread1 extends Thread {
+	
+	@Override
+    public void run() {
+		System.out.println("In run method, name:  : " + Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args) {
+		System.out.println("In main method, name: " + Thread.currentThread().getName());
+		Thread t = new Mythread1();
+		t.run();
+
+	}
+
+}
